847d13aaad7d6b0e800084f433361d3030d32bc8
Kate-Polishchuk <98930140+Kate-Polishchuk@users.noreply.github.com>
2022-02-03 23:22:38 -0500

    Update Task 2 - for 02.03.py

--- Task 2 - for 02.03.py
+++ Task 2 - for 02.03.py
@@ -1,6 +1,6 @@
-# 'It's a good day'
+# It's a good day
 print('It\'s a good day')
 print('It\'s', 'a', 'good', 'day', sep='..')
-# 'How..was..your..day?'
+# How..was..your..day?
 print('How', 'was', 'your', 'day', sep='..', end='?')
 
