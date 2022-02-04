d70ccbded515a2c97950e3e574f7a6b4078082b0
Kate-Polishchuk <98930140+Kate-Polishchuk@users.noreply.github.com>
2022-02-03 20:42:59 -0500

    Homework for 02.03 class

---
+++ Task 2 - for 02.03.py
@@ -0,0 +1,6 @@
+# 'It's a good day'
+print('It\'s a good day')
+print('It\'s', 'a', 'good', 'day', sep='..')
+# 'How..was..your..day?'
+print('How', 'was', 'your', 'day', sep='..', end='?')
+
---
+++ Task 3 - for 02.03.py
@@ -0,0 +1,2 @@
+print('#######\n#\t#\n#\t#\n#\t#\n#######'.expandtabs(6))
+print('#\t#\n#\t#\n#######\n#\t#\n#\t#'.expandtabs(6))
