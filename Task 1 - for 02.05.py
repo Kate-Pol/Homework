a9604fc6fd2d1059530d858e1f675af8486b2c99
Kate-Polishchuk <98930140+Kate-Polishchuk@users.noreply.github.com>
2022-02-06 17:10:04 -0500

    02.05 lesson
    Homework for 02.05 lesson

---
+++ Task 1 - for 02.05.py
@@ -0,0 +1,5 @@
+weight = 63
+height = 1.7
+bmi = weight / (height ** 2)
+# BMI is normal if > 18.5 and < 25
+print('Your BMI is normal:', 18.5 <= bmi <= 25)
---
+++ Task 2 - for 02.05.py
@@ -0,0 +1,7 @@
+cell_number = '380501234567'
+total_numbers = 12
+cell_area_code = '380'
+print(len(cell_number) == total_numbers)
+print(cell_number.isdecimal())
+print(cell_number[0:3] == cell_area_code)
+
